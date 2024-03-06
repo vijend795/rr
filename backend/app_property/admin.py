a1f2e2e779241d0b99e412719cb884cb1923c484
vijend795 <singh.vijendra05@gmail.com>
2024-03-06 21:24:54 +1300

    update

--- backend/app_property/admin.py
+++ backend/app_property/admin.py
@@ -15,6 +15,37 @@ class PropertyAdmin(admin.ModelAdmin):
     model=Property
     list_display=('custom_id',)
     inlines=[SubPropertyInline]
+    
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.select_related(
+                                            'unit',
+                                            'plot',
+                                            'floor',
+                                            'building',
+                                            'tower',
+                                            ) \
+                           .prefetch_related(
+                               'sub_properties',
+                               'unit__unit_floor_relationships__floor',
+                                'unit__unit_floor_relationships__floor__floor_tower_relationships',
+                                'unit__unit_floor_relationships__floor__floor_plot_relationships__plot',
+                                'unit__unit_floor_relationships__floor__floor_plot_relationships__plot__plot_building_relationships__building',
+                                'unit__unit_floor_relationships__floor__floor_plot_relationships__plot__plot_building_relationships__building__area',
+                                'unit__unit_floor_relationships__floor__floor_plot_relationships__plot__plot_building_relationships__building__area__area_locality_relations__locality',
+                                'unit__unit_floor_relationships__floor__floor_plot_relationships__plot__plot_building_relationships__building__area__area_locality_relations__locality__city__state',
+                                'unit__unit_floor_relationships__floor__floor_tower_relationships__tower',
+                                'unit__unit_floor_relationships__floor__floor_tower_relationships__tower__tower_building_relationships__building',
+                                
+                                'floor__floor_plot_relationships__plot__plot_building_relationships__building__area__area_locality_relations__locality__city__state',
+                                'floor__floor_tower_relationships__tower__tower_building_relationships__building__area__area_locality_relations__locality__city__state',
+                                'tower__tower_building_relationships__building__area__area_locality_relations__locality__city__state',
+                                'building__area__area_locality_relations__locality__city__state',
+                                'building__plot_building_relationships__plot__area__area_locality_relations__locality__city__state',
+                                'plot__area__area_locality_relations__locality__city__state'
+                                
+                                )
+        return queryset
 
 class SubPropertyAdmin(admin.ModelAdmin):
     model=SubProperty
